0966538a5325809d5c80df2ef001045abee57b22
yuzhiyuan413 <1048921160@qq.com>
2019-05-18 17:36:59 +0800

    no message

---
+++ src/main/java/com/example/webtest/model/Permission.java
@@ -0,0 +1,92 @@
+package com.example.webtest.model;
+
+import com.example.webtest.bean.MapperBean;
+import java.util.Date;
+
+public class Permission extends MapperBean {
+    private Long id;
+
+    private String permissionCode;
+
+    private Byte functionType;
+
+    private Byte businessType;
+
+    private String permissionName;
+
+    private String description;
+
+    private Byte status;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPermissionCode() {
+        return permissionCode;
+    }
+
+    public void setPermissionCode(String permissionCode) {
+        this.permissionCode = permissionCode;
+    }
+
+    public Byte getFunctionType() {
+        return functionType;
+    }
+
+    public void setFunctionType(Byte functionType) {
+        this.functionType = functionType;
+    }
+
+    public Byte getBusinessType() {
+        return businessType;
+    }
+
+    public void setBusinessType(Byte businessType) {
+        this.businessType = businessType;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
